d3cc510b7f74eb97c2da46a94ec4c6ada622953f
Nathaniel Russell <46272571+nater1983@users.noreply.github.com>
2023-11-30 03:58:35 +0000

    Update and rename localed.h to timedated.h

--- src/timedated.h
+++ src/timedated.h
@@ -14,40 +14,18 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-  Modified in 2019 by Pierre Labastie. See git log
 */
 
-#ifndef B_LOCALED_H
-#define B_LOCALED_H
+#ifndef OPENRC_TIMEDATED_H
+#define OPENRC_TIMEDATED_H
 
 #include <glib.h>
-#include <gio/gio.h>
-
-/**
- * SECTION: localed
- * @short_description: where all the interface to D-Bus is done
- * @title: Localed functions
- * @include: localed.h
- *
- * This is where all the work is done:
- * - reading and storing settings
- * - interaction with D-Bus
- * - calling polkit for authorization
- *
- * Only two functions are public: #localed_init to initiate the connection
- * with D-Bus and readd the settings from files, and #localed_destoy, for
- * garbage collection at exit.
- */
 
 void
-localed_init (gboolean _read_only,
-	      const gchar *kbd_model_map,
-	      const gchar *localeconfig,
-	      const gchar *keyboardconfig,
-	      const gchar *xkbdconfig);
+timedated_init (gboolean read_only,
+                const gchar *_ntp_preferred_service);
 
 void
-localed_destroy (void);
+timedated_destroy (void);
 
 #endif
